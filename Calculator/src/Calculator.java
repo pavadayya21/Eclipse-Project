570934634d1452f8cf6878ee5ae1406341a53c9f
Pavadayya <admin@gmail.com>
2020-02-28 19:43:33 +0530

    first commit uploading all the files

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,26 @@
+
+public class Calculator {
+	public int add(int num1, int num2)
+	{
+		int r = num1 + num2;
+		return r;
+		
+	}
+	public int sub(int num1, int num2)
+	{
+		int r = num1 - num2;
+		return r;
+		
+	}
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		int a = cal.add(2,4);
+		int b = cal.sub(4,2);
+	 System.out.println("Adding two numbers:" +a);
+	 System.out.println("Subtracting two numbers:" +b);
+	 
+	 
+		
+	}
+}
+
