12db3945bb5079043de90a6f7c3e03a237678411
Pavadayya <pavadayya@gmail.com>
2020-03-04 11:23:58 +0530

    commit changes calcutor

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -12,15 +12,23 @@ public class Calculator {
 		return r;
 		
 	}
+	
+	public int mul(int num1, int num2)
+	{
+		int r = num1 * num2;
+		return r;
+	}
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
 		int a = cal.add(2,4);
 		int b = cal.sub(4,2);
+		int c = cal.mul(5, 6);
 	 System.out.println("Adding two numbers:" +a);
 	 System.out.println("Subtracting two numbers:" +b);
+	 System.out.println("Multipliying two numbers:"+c);
 	 
-	 
-		
 	}
 }
+
+
 
